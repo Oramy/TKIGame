fa621c0f2efe78b027de11f05c69adebd8efeda0
Amaury Butaux <ymarua@hotmail.fr>
2019-04-26 21:08:02 +0200

    nav test

---
+++ Assets/TestScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TestScript : MonoBehaviour
+{
+    public Transform target;
+    private NavMeshAgent navMesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        navMesh.destination = target.position;
+    }
+}
